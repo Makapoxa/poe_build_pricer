f3d10268a8f948f0e2d7d194db30c35ea2458b0a
Dmitry Makarov <makarocha@gmail.com>
2021-10-11 20:29:33 +0400

    Add credo and more deps

--- mix.exs
+++ mix.exs
@@ -38,7 +38,11 @@ defmodule PoeBuildPricer.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:typed_struct, "~> 0.2.1"},
+      {:tesla, "~> 1.4"},
+      {:hackney, "~> 1.17"}
     ]
   end
 
